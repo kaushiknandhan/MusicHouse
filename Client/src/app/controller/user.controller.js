c06ae0e827dfc5b77e63ec498fcf7aa5ccc441b8
kaushik nandhan injamuri <kaushik nandhan injamuri>
2017-04-05 16:14:36 -0700

    feat(MusicHouse): add user functionality

--- Client/src/app/controller/user.controller.js
+++ Client/src/app/controller/user.controller.js
@@ -1,3 +1,43 @@
 /**
  * Created by kaushik nandhan on 3/18/2017.
  */
+(function () {
+    'use strict';
+    angular.module('musichouse')
+        .controller('userController',userController);
+
+    userController.$inject = ['userService','$location'];
+    function userController(userService,$location){
+        var userVm = this;
+        userVm.user = {};
+        userVm.newUser = {};
+        userVm.login = login;
+        userVm.signup = signup;
+
+        // login functionality
+        function login(){
+            userService.loginUser(userVm.user)
+                .then(function (user) {
+                    userVm.user = user;
+                    console.dir(userVm.user);
+                    $location.path('/home');
+                },function (error) {
+                    console.log('error in user controller'+error);
+                });
+        }
+
+        // signUp functionaloty
+
+        function signup(){
+            userVm.newUser.createdOn = (new Date).getTime();
+            userService.signUp(userVm.newUser)
+                .then(function (newUser) {
+                    userVm.newUser =  newUser;
+                    console.dir(userVm.newUser);
+                    $location.path('/login');
+                },function (error) {
+                    console.log('error @ sgnup functionality in user controller :'+error);
+                });
+        }
+    }
+})();
--- Client/src/app/service/user.service.js
+++ Client/src/app/service/user.service.js
@@ -1,3 +1,33 @@
 /**
  * Created by kaushik nandhan on 3/18/2017.
  */
+(function () {
+    'use strict';
+    angular.module('musichouse')
+        .service('userService',userService);
+
+    userService.$inject = ['$http','$q'];
+    function userService($http,$q){
+        var userVm = this;
+        userVm.loginUser = loginUser;
+        userVm.signUp = signUp;
+
+
+        function loginUser(userObj){
+            return $http.post('http://localhost:8080/musichouse/api/users/login',userObj)
+                .then(successFn,errorFn);
+        }
+
+        function signUp(newUser){
+            return $http.post('http://localhost:8080/musichouse/api/users/register',newUser)
+                .then(successFn,errorFn);
+        }
+
+        function successFn(response){
+            return response.data;
+        }
+        function errorFn(response) {
+            return $q.reject('some error from server: '+response.statusText);
+        }
+    }
+})();
