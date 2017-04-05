98474a11e933f42f281ed4398f841709e0ac620e
kaushik nandhan injamuri <kaushik nandhan injamuri>
2017-04-05 16:16:49 -0700

    feat(MusicHouse): add cartitem functionality

--- Client/src/app/controller/cartItem.controller.js
+++ Client/src/app/controller/cartItem.controller.js
@@ -1,3 +1,21 @@
 /**
  * Created by kaushik nandhan on 3/18/2017.
  */
+(function () {
+    'use strict';
+    angular.module('musichouse')
+        .controller('cartItemController',cartItemController);
+
+    cartItemController.$inject = ['cartItemService'];
+    function cartItemController(cartItemService){
+        var cartItemVm = this;
+        cartItemVm.cartItems = [];
+
+        init();
+
+        function init(){
+            cartItemVm.cartItems = cartItemService.getCartItems();
+        }
+    }
+
+})();
--- Client/src/app/service/cartItem.service.js
+++ Client/src/app/service/cartItem.service.js
@@ -1,3 +1,28 @@
 /**
  * Created by kaushik nandhan on 3/18/2017.
  */
+(function () {
+    'use strict';
+    angular.module('musichouse')
+        .service('cartItemService',cartItemService);
+
+    cartItemService.$inject = ['$http','$q'];
+    function cartItemService($http,$q){
+        var cartItemVm = this;
+        cartItemVm.cart = [];
+        cartItemVm.addCartItems = addCartItems;
+        cartItemVm.getCartItems = getCartItems;
+
+        function addCartItems(product){
+            cartItemVm.cart.push(product);
+            console.dir(cartItemVm.cart);
+            return true;
+        }
+
+        function getCartItems() {
+            return cartItemVm.cart;
+        }
+
+
+    }
+})();
