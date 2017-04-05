f1608ef8068ff67a8bf12e7a2a34546d3ed1e6d8
kaushik nandhan injamuri <kaushik nandhan injamuri>
2017-04-05 16:17:33 -0700

    feat(MusicHouse): add routing functionality

--- Client/src/app/app.js
+++ Client/src/app/app.js
@@ -3,15 +3,42 @@
  */
 (function(){
     'use strict';
-    angular.module('musichouse',['ngRoute'])
+    angular.module('musichouse',['ui.bootstrap','ngRoute','ngAnimate','ngTouch','ngMessages'])
         .config(moduleConfig);
-    function moduleConfig($routeProvider) {
+
+    function moduleConfig($routeProvider,$locationProvider) {
+        $locationProvider.hashPrefix('');
         $routeProvider
             .when('/home',{
                 templateUrl:'app/view/home.tmpl.html'
             })
+            .when('/signup',{
+                templateUrl:'app/view/signup.tmpl.html',
+                controller:'userController',
+                controllerAs:'userVm'
+            })
+            .when('/login',{
+                templateUrl:'app/view/login.tmpl.html',
+                controller:'userController',
+                controllerAs:'userVm'
+            })
+            .when('/products',{
+                templateUrl:'app/view/products.tmpl.html',
+                controller:'productController',
+                controllerAs:'productVm'
+            })
+            .when('/products/:productId',{
+                templateUrl:'app/view/productInfo.tmpl.html',
+                controller:'productInfoController',
+                controllerAs:'productInfoVm'
+            })
+            .when('/cart',{
+                templateUrl:'app/view/cartItems.tmpl.html',
+                controller:'cartItemController',
+                controllerAs:'cartItemVm'
+            })
             .otherwise({
                 redirectTo:'/home'
-            });
+            })
     }
 })();
